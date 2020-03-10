31ac5f674ee69fb7f47f667495a7ab37273836bd
Diego Andrés <diegoandres_cortes@outlook.com>
2020-03-10 00:58:53 -0600

    less... stuff
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Themes/default/TeamPage.template.php
+++ Themes/default/TeamPage.template.php
@@ -30,13 +30,11 @@ function template_teampage_view()
 	global $context;
 
 	// Groups
-	if (!empty($context['teampage']['team']))
-	{
+	if (empty($context['teampage']['is_text']))
 		echo '
 		<div id="tp_main_box">
 			', display_members(), '
 		</div>';
-	}
 	// Text
 	else
 		echo '
@@ -52,95 +50,77 @@ function template_TeamPage_below()
 	echo $context['teampage']['copyright'];
 }
 
-function display_members($placement = 'left')
+function display_members()
 {
 	global $context, $txt, $scripturl, $modSettings, $settings, $boardurl;
 
-	// Load groups
-	if (!empty($context['teampage']['team']))
+	// Blocks
+	foreach($context['teampage']['team'] as $placement => $groups)
 	{
-		// Blocks
-		foreach($context['teampage']['team'] as $placement => $groups)
+		echo '
+		<div class="roundframe" id="tp_block_'. $placement . '">';
+		// Groups
+		foreach($groups as $group)
 		{
-			echo '
-			<div class="roundframe" id="tp_block_'. $placement . '">';
-
-			// Groups
-			foreach($groups as $group)
+			echo  '
+			<div class="tp_group_container">
+				<div class="title_bar">
+					<h4 class="titlebg">
+						', $group['group_name'], '
+					</h4>
+				</div>';
+			// Description and emmm... Badge?
+			if (!empty($modSettings['TeamPage_show_badges']) || (!empty($modSettings['TeamPage_show_description']) && !empty($group['description'])))
 			{
-				echo  '
-				<div class="tp_group_container">
-					<div class="title_bar">
-						<h4 class="titlebg">
-							', $group['group_name'], '
-						</h4>
-					</div>';
-
-				// Description and emmm... Badge?
-				if (!empty($modSettings['TeamPage_show_badges']) || (!empty($modSettings['TeamPage_show_description']) && !empty($group['description'])))
-				{
-					echo '
-					<div class="information">
-						', !empty($modSettings['TeamPage_show_description']) && !empty($group['description']) ? '<span class="tp_group_description">'. $group['description'].'</span>' : '', '
-						', display_badge($group['icons']), '
-					</div>';
-				}
-
-				// Users
-				if (!empty($group['members']))
-				{
-					echo '
-					<ul>';
-					
-					// Members
-					foreach($group['members'] as $user)
-					{
-						echo '
-						<li', ($placement != 'bottom' ? ' class="windowbg"' : '') , '>
-							<img class="tp_avatar" src="', !empty($user['avatar']['href']) ? $user['avatar']['href'] : $boardurl. '/avatars/default.png', '" alt="" />
-							<h2 class="tp_user_name">
-								<a href="', $scripturl, '?action=profile;u=', $user['id_member'], '">', $user['real_name'], '</a>
-								', !empty($modSettings['TeamPage_show_custom']) && !empty($user['usertitle']) ? ' - <strong>'. $user['usertitle']. '</strong>' : '', '
-							</h2>
-							<span class="tp_user_info">
-								', !empty($modSettings['TeamPage_show_personal']) && !empty($user['personal_text']) ? '
-								<span class="tp_user_personal">
-									<strong>' . $txt['personal_text'] . ': </strong><i>' . $user['personal_text']. '</i>
-								</span>' : '', '
-								', !empty($modSettings['TeamPage_show_registered']) ? '
-								<span class="tp_user_joined">
-									<strong>' . $txt['TeamPage_date_registered'] . ': </strong>' . timeformat($user['date_registered']). '
-								</span>' : '', '
-								', !empty($modSettings['TeamPage_show_login']) ? '
-								<span class="tp_user_joined">
-									<strong>' . $txt['TeamPage_last_login'] . ': </strong>' . timeformat($user['last_login']). '
-								</span>' : '', '
-								', !empty($modSettings['TeamPage_show_posts']) ? '
-								<span class="tp_user_joined">
-									<strong>' . $txt['posts'] . ': </strong>' . $user['posts']. '
-								</span>' : '', '
-								', !empty($modSettings['TeamPage_show_website']) && !empty($user['website_url']) ? '
-								<span class="tp_user_joined">
-									<strong>' . $txt['TeamPage_website'] . ': </strong><a href="' . $user['website_url']. '" target="_blank" rel="noopener">' . $user['website_title']. '</a>
-								</span>' : '', '
-							</span>
-						</li>';
-					}
-					echo '
-					</ul>';
-				}
-				else
-					echo '
-					<div class="windowbg">
-						', $txt['TeamPage_no_members'], '
-					</div>';
-
 				echo '
+				<div class="information">
+					', !empty($modSettings['TeamPage_show_description']) && !empty($group['description']) ? '<span class="tp_group_description">'. $group['description'].'</span>' : '', '
+					', display_badge($group['icons']), '
 				</div>';
 			}
-		echo '
+			echo '
+				<ul>';
+				
+				// Members
+				foreach($group['members'] as $user)
+				{
+					echo '
+					<li', ($placement != 'bottom' ? ' class="windowbg"' : '') , '>
+						<img class="tp_avatar" src="', !empty($user['avatar']['href']) ? $user['avatar']['href'] : $boardurl. '/avatars/default.png', '" alt="" />
+						<h2 class="tp_user_name">
+							<a href="', $scripturl, '?action=profile;u=', $user['id_member'], '">', $user['real_name'], '</a>
+							', !empty($modSettings['TeamPage_show_custom']) && !empty($user['usertitle']) ? ' - <strong>'. $user['usertitle']. '</strong>' : '', '
+						</h2>
+						<span class="tp_user_info">
+							', !empty($modSettings['TeamPage_show_personal']) && !empty($user['personal_text']) ? '
+							<span class="tp_user_personal">
+								<strong>' . $txt['personal_text'] . ': </strong><i>' . $user['personal_text']. '</i>
+							</span>' : '', '
+							', !empty($modSettings['TeamPage_show_registered']) ? '
+							<span class="tp_user_joined">
+								<strong>' . $txt['TeamPage_date_registered'] . ': </strong>' . timeformat($user['date_registered']). '
+							</span>' : '', '
+							', !empty($modSettings['TeamPage_show_login']) ? '
+							<span class="tp_user_joined">
+								<strong>' . $txt['TeamPage_last_login'] . ': </strong>' . timeformat($user['last_login']). '
+							</span>' : '', '
+							', !empty($modSettings['TeamPage_show_posts']) ? '
+							<span class="tp_user_joined">
+								<strong>' . $txt['posts'] . ': </strong>' . $user['posts']. '
+							</span>' : '', '
+							', !empty($modSettings['TeamPage_show_website']) && !empty($user['website_url']) ? '
+							<span class="tp_user_joined">
+								<strong>' . $txt['TeamPage_website'] . ': </strong><a href="' . $user['website_url']. '" target="_blank" rel="noopener">' . $user['website_title']. '</a>
+							</span>' : '', '
+						</span>
+					</li>';
+				}
+				echo '
+				</ul>
 			</div>';
 		}
+		echo '
+		</div>';
 	}
 }
 
