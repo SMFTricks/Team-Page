58a72de31796ffedd436de9898d9f4444b9b53ce
Diego Andrés <diegoandres_cortes@outlook.com>
2022-01-25 12:07:46 -0600

    v5.2
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Sources/TeamPage/TeamPage.php
+++ Sources/TeamPage/TeamPage.php
@@ -16,7 +16,7 @@ if (!defined('SMF'))
 class TeamPage
 {
 	public static $name = 'TeamPage';
-	public static $version = '5.1.3';
+	public static $version = '5.2';
 
 	public static function initialize()
 	{
