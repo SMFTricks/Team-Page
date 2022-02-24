84ae016dc847c9740ca525740235d5745e6ffae4
Diego Andrés <diegoandres_cortes@outlook.com>
2022-02-23 22:07:40 -0600

    fix queries
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Sources/TeamPage/Moderators.php
+++ Sources/TeamPage/Moderators.php
@@ -19,20 +19,17 @@ class Moderators
 	public  static $mods_columns = ['md.id_board', 'md.id_member'];
 	public  static $cats_columns = ['c.id_cat', 'c.name AS cat_name', 'c.cat_order'];
 	public  static $boards_columns = ['b.id_board', 'b.board_order', 'b.id_cat', 'b.name', 'b.child_level'];
-	private static $additional_query = '';
-	private static $fields_data = [];
-	private static $fields_type = '';
+	private $fields_data = [];
+	private $fields_type = '';
 
-	public static function Save()
+	public function Save()
 	{
-		global $smcFunc, $txt;
-
 		// Unlucky
-		if (!isset($_REQUEST['id']) || empty($_REQUEST['id']) || empty(Helper::Find(Pages::$table . ' AS cp', 'cp.id_page', $_REQUEST['id'])))
-			fatal_error($txt['TeamPage_page_noexist'], false);
+		if (!isset($_REQUEST['id']) || empty($_REQUEST['id']) || empty(Helper::Get('', '', '', Pages::$table . ' AS cp', ['cp.id_page'], 'WHERE cp.id_page = {int:page}', true, '', ['page' => (int) $_REQUEST['id']])))
+			fatal_lang_error('TeamPage_page_noexist', false);
 
 		// Data
-		self::$fields_data = [
+		$this->fields_data = [
 			'id_page' => (int) $_REQUEST['id'],
 			'page_boards' => (string) isset($_REQUEST['boardset']) && !empty($_REQUEST['boardset']) && is_array($_REQUEST['boardset']) ? implode(',', $_REQUEST['boardset']) : '',
 			'mods_style' => (int) isset($_REQUEST['mod_style']) ? $_REQUEST['mod_style'] : 0,
@@ -40,11 +37,11 @@ class Moderators
 		checkSession();
 
 		// Type
-		foreach(self::$fields_data as $column => $type)
-			self::$fields_type .= $column . ' = {'.str_replace('integer', 'int', gettype($type)).':'.$column.'}, ';
+		foreach($this->fields_data as $column => $type)
+			$this->fields_type .= $column . ' = {'.str_replace('integer', 'int', gettype($type)).':'.$column.'}, ';
 
 		// Update
-		Helper::Update(Pages::$table, self::$fields_data, self::$fields_type, 'WHERE id_page = ' . self::$fields_data['id_page']);
+		Helper::Update(Pages::$table, $this->fields_data, $this->fields_type, 'WHERE id_page = {int:id_page}');
 
 		redirectexit('action=admin;area=teampage;sa=pages;updated');
 	}
